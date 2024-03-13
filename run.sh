fba6bfd7a23f08d4f01f839906fcfc17faf5a86a
Max Bender <maxbbender@gmail.com>
2024-03-13 17:58:56 -0400

    added run file

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo  ./Installomator/Installomator.sh googlechromepkg DEBUG=0
+echo 'Add the Google Chrome configuration profile to Mac : System Settings -> Privacy & Security -> Profiles -> Add (+) -> ChromeConfig.mobileconfig'
